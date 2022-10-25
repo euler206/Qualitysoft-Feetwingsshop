1bbe1c0e85bc672d08158ae793bb30cd7a2ff593
Viktor Grajales <viagramo2011@gmail.com>
2022-10-25 17:24:10 -0500

    Pagina Principal con Novedades

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,9 @@ import Container from "react-bootstrap/Container";
 import Inicio from "./pages/Inicio";
 import NavbarComponent from "./components/NavbarComponent";
 import Hombre from "./components/Hombre";
-import { Footer } from "./components/Footer";
+import  Footer  from "./components/Footer";
+import CrearProducto from "./pages/CrearProducto"
+
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
       <Container className="fondo border py-4 px-4 shadow-lg p-3 mb-5 bg-white rounded">
         <Routes>
           <Route exact path="/" element={<Inicio />} /> 
-          <Route exact path="/Hombre" element={<Hombre />} />     
+          <Route exact path="/Hombre" element={<Hombre />} />    
+          <Route exact path="/crearproducto" element={<CrearProducto /> } /> 
                  
         </Routes>
       </Container>
--- front/src/components/NavbarComponent.js
+++ front/src/components/NavbarComponent.js
@@ -1,20 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import { Form, Button, Navbar, Badge } from "react-bootstrap/";
+import {
+  Form,
+  Button,
+  Navbar,
+  Badge,
+  Image,
+  NavDropdown,
+} from "react-bootstrap/";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import logo from "../recursos/images/logo.png";
 
 function NavbarComponent() {
   return (
     <>
       <Navbar variant="dark" className="bg-NavBar">
         <Container>
+          <Navbar.Brand href="#home">
+            <Image width={70} src={logo} />
+          </Navbar.Brand>
           <Navbar.Brand href="#home">FeetWings Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Catalogo</Nav.Link>
-            <Nav.Link href="#pricing">Hombres</Nav.Link>
-            <Nav.Link href="#pricing">Mujer</Nav.Link>
+            <NavDropdown title="Catalogo" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Hombre</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Mujer</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Niños</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#pricing">Ofertas</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -26,14 +40,14 @@ function NavbarComponent() {
             <Button variant="outline-light">Search</Button>
           </Form>
           <Nav className="me-auto">
-          <IconContext.Provider value={{ color: "#fff", className: "myicons" }}>
-            <FaUser />
-            <FaShoppingCart/><Badge  bg="danger">2</Badge>
-
+            <IconContext.Provider
+              value={{ color: "#fff", className: "myicons" }}
+            >
+              <FaUser />
+              <FaShoppingCart />
+              <Badge bg="danger">2</Badge>
             </IconContext.Provider>
           </Nav>
-
-          
         </Container>
       </Navbar>
     </>
