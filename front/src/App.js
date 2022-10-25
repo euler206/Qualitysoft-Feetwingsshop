8d66222a4db1dbe77aeef4068cace83e643e0975
Carlos Rocha <carocha2008@gmail.com>
2022-10-24 22:16:02 -0500

    Novedades centrar inicio

--- front/src/App.js
+++ front/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Inicio from "./pages/Inicio";
 import NavbarComponent from "./components/NavbarComponent";
+import Hombre from "./components/Hombre";
 import { Footer } from "./components/Footer";
-import { Novedades } from "./components/Novedades";
 
 function App() {
   return (
@@ -15,13 +15,13 @@ function App() {
     <Container className="">
       <Container className="fondo border py-4 px-4 shadow-lg p-3 mb-5 bg-white rounded">
         <Routes>
-          <Route exact path="/" element={<Inicio />} />       
+          <Route exact path="/" element={<Inicio />} /> 
+          <Route exact path="/Hombre" element={<Hombre />} />     
                  
         </Routes>
       </Container>
     </Container>
     <Footer/>
-    <Novedades/>
   </Router>
   );
 }
--- front/src/pages/Inicio.js
+++ front/src/pages/Inicio.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BannerOfertas from '../components/BannerOfertas'
+import Novedades from '../components/Novedades'
 
 
 
 function Inicio() {
   return (
-    <div className='d-flex justify-content-center'>
-      <BannerOfertas/>      
+    <div className='text-center'>
+      <BannerOfertas/>
+      <Novedades/>     
     </div>
   )
 }
