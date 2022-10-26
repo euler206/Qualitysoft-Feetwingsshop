cdd063f81809166b7315c19479c949d846c2080d
Euler Basante <euler206@gmail.com>
2022-10-25 22:21:20 -0500

    commit ventas

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Inicio from "./pages/Inicio";
+//import Inicio from "./pages/Inicio";
+import ListarVentas from "./pages/ventas";
 import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
@@ -13,7 +14,14 @@ function App() {
     <Container className="">
       <Container className="fondo border py-4 px-4 shadow-lg p-3 mb-5 bg-white rounded">
         <Routes>
-          <Route exact path="/" element={<Inicio />} />       
+          {/*
+          <Route exact path="/" element={<Inicio />} /> 
+  */}
+
+
+          <Route exact path="/" element = {<ListarVentas />} /> 
+
+      
                  
         </Routes>
       </Container>
