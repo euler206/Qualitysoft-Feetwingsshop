203b8970c3f9860354074202131a9a9da42fe70a
Euler Basante <euler206@gmail.com>
2022-10-26 22:05:47 -0500

    cargar ventas

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-//import Inicio from "./pages/Inicio";
+import Inicio from "./pages/Inicio";
 import ListarVentas from "./pages/ventas";
 import NavbarComponent from "./components/NavbarComponent";
 import Hombre from "./components/Hombre";
@@ -19,7 +19,6 @@ function App() {
       <Container className="fondo border py-4 px-4 shadow-lg p-3 mb-5 bg-white rounded">
         <Routes>
      
-                 
           <Route exact path="/" element={<Inicio />} />       
           <Route exact path="/crearproducto" element={<CrearProducto /> } />
           <Route exact path="/Hombre" element={<Hombre />} />    
