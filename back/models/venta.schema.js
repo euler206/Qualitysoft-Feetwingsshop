88e6fd7ccb44f86d02c93ab5abfa40d1b8158719
Euler Basante <euler206@gmail.com>
2022-11-08 22:19:14 -0500

    services ventas

--- back/models/venta.schema.js
+++ back/models/venta.schema.js
@@ -23,28 +23,9 @@ const ventaSchema = new Schema({
         idProducto:String,
         cantidad:Number,
         precio:Number,
-        subtotal:(function(){
-            return this.cantidad * this.precio
-        })()
     }],
     valor : Number
     
-    // valor   NO FUNCA ====>  QUEDA EN REMOJO
-    /*
-    valor: (function(){
-        let total = 0
-        this.detalleCompra.forEach(element => {
-            total += element.subtotal
-        });
-        return total
-    })()
-    */
-
-    /*
-    valor: this.detalleCompra.forEach(element => {
-            total += element.precio * element.cantidad;
-        })()
-    */
 })
 
 const Venta = model("Venta",ventaSchema)
