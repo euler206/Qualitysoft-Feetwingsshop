77474689fdf784fdff59f1352dac384e13d65da4
Euler Basante <euler206@gmail.com>
2022-11-20 21:47:40 -0500

    Docker

--- back/config/mongoConnect.js
+++ back/config/mongoConnect.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'config/config.env' });
 
-const connectionString =process.env.DB_LOCAL_URI;      // CONEXION A MONGO LOCAL
-//const connectionString =process.env.DB_ATLAS_URI;    // CONEXION A MONGO ATLAS
+//                        Conexion  a MONGO ATLAS  // Conexion a MONGO LOCAL
+const connectionString =(process.env.DB_ATLAS_URI || process.env.DB_LOCAL_URI);      
 
 const dbconnect = mongoose.connect(connectionString,{useNewUrlParser: true})
   .then(con => {
